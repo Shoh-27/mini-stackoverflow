ec070e59ac67845df84e966c20c019b3f88face8
Shohrux <shohruxnizomov170@gmail.com>
2025-09-21 13:35:03 +0500

    seeder orqali categorylar qoshildi

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,29 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            'PHP',
+            'Laravel',
+            'JavaScript',
+            'Vue.js',
+            'React',
+            'CSS',
+            'HTML',
+            'MySQL',
+            'PostgreSQL',
+            'DevOps',
+            'Linux',
+            'Docker',
+            'Git',
+            'Algorithms',
+            'Data Structures'
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+            ]);
+        }
     }
 }
